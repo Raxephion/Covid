897057199326c2f391892ef3775c97bccd2e4be9
Raxephion <Raxephion@gmail.com>
2020-11-07 10:19:05 +0200

    Add files via upload

---
+++ Covid_ZA.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  7 10:15:20 2020
+
+@author: Pierre-Henri Rossouw
+"""
+
+import pandas as pd
+
+url = 'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx'
+df_raw = pd.read_excel(url)
+
+df_raw['countriesAndTerritories'].unique()
+df = df_raw
+
+df = df[df['countriesAndTerritories']=='South_Africa']
+
+df = df.sort_values(['month', 'deaths' ,'cases'], ascending=[1, 1, 1])
+
+df['month'] = pd.to_datetime(df[['year', 'month' ,'day']])
+
+
+ax = df.plot(x='month', y = ['cases','deaths'], title = 'Covid Statistics for South Africa', figsize=(10,7), grid=True)
